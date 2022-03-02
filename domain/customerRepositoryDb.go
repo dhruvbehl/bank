81b5db9af55952f30664cf4a700451bafaac48ef
dhruv behl <dhruvbhl@gmail.com>
2022-03-02 21:59:34 +0530

    Adding support for SQLx

--- domain/customerRepositoryDb.go
+++ domain/customerRepositoryDb.go
@@ -8,40 +8,30 @@ import (
 	"github.com/dhruvbehl/bank/errors"
 	"github.com/dhruvbehl/bank/logger"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 )
 
 type CustomerRepositoryDb struct {
-	client *sql.DB
+	client *sqlx.DB
 }
 
 func (d CustomerRepositoryDb) FindAll() ([]Customer, *errors.AppError) {
-	sqlQuery := "select * from customers"
-	rows, err := d.client.Query(sqlQuery)
-	if err != nil {
-		logger.Error(fmt.Sprintf("[error while querying db] %v", err.Error()))
-		return nil, errors.NewInternalServerError("unexpected database error")
-	}
-
 	customers := make([]Customer, 0)
-	for rows.Next() {
-		var c Customer
-		err := rows.Scan(&c.Id, &c.Name, &c.DateOfBirth, &c.City, &c.Zipcode, &c.Status)
-		if err != nil {
-			logger.Error(fmt.Sprintf("[error while scanning results] %v",err.Error()))
-			return nil, errors.NewInternalServerError("error while scanning results")
-		}
-		customers = append(customers, c)
+	sqlQuery := "select * from customers"
+	err := d.client.Select(&customers, sqlQuery)
+	if err != nil {
+		logger.Error(fmt.Sprintf("[error while scanning results] %v",err.Error()))
+		return nil, errors.NewInternalServerError("error while scanning results")
 	}
 	logger.Info("returning results for FindAll query")
 	return customers, nil
 }
 
 func (d CustomerRepositoryDb) FindById(id string) (*Customer, *errors.AppError) {
-	sqlQuery := fmt.Sprintf("select * from customers where customer_id=%v", id)
-	row := d.client.QueryRow(sqlQuery)
-
 	c := Customer{}
-	err := row.Scan(&c.Id, &c.Name, &c.DateOfBirth, &c.City, &c.Zipcode, &c.Status)
+	sqlQuery := fmt.Sprintf("select * from customers where customer_id=%v", id)
+	err := d.client.Get(&c, sqlQuery)
+
 	if err != nil {
 		if err == sql.ErrNoRows {
 			logger.Error(fmt.Sprintf("[error while scanning results] %v",err.Error()))
@@ -55,32 +45,23 @@ func (d CustomerRepositoryDb) FindById(id string) (*Customer, *errors.AppError) 
 }
 
 func (d CustomerRepositoryDb) FindByStatus(status string) ([]Customer, *errors.AppError) {
-	sqlQuery := fmt.Sprintf("SELECT * FROM customers where status=%v", status)
-	rows, err := d.client.Query(sqlQuery)
-	if err != nil {
-		logger.Error(fmt.Sprintf("[error while querying db] %v", err.Error()))
-		return nil, errors.NewInternalServerError("unexpected database error")
-	}
 	customers := make([]Customer, 0)
-	for rows.Next() {
-		var c Customer
-		err = rows.Scan(&c.Id, &c.Name, &c.DateOfBirth, &c.City, &c.Zipcode, &c.Status)
-		if err != nil {
-			logger.Error(fmt.Sprintf("[error while scanning results] %v",err.Error()))
-			return nil, errors.NewInternalServerError("error while scanning results")
-		}
-		customers = append(customers, c)
+	sqlQuery := fmt.Sprintf("SELECT * FROM customers where status=%v", status)
+	err := d.client.Select(&customers, sqlQuery)
+	if err != nil {
+		logger.Error(fmt.Sprintf("[error while scanning results] %v",err.Error()))
+		return nil, errors.NewInternalServerError("error while scanning results")
 	}
 	logger.Info("returning results for FindByStatus query")
 	return customers, nil
 }
 
 func NewCustomerRepositoryDb() CustomerRepositoryDb {
-	client, err := sql.Open("mysql", "root:qs@tcp(localhost:3306)/banking")
+	client, err := sqlx.Open("mysql", "root:qs@tcp(localhost:3306)/banking")
 	if err != nil {
 		panic(err)
 	}
-	// See "Important settings" section.
+
 	client.SetConnMaxLifetime(time.Minute * 3)
 	client.SetMaxOpenConns(10)
 	client.SetMaxIdleConns(10)
